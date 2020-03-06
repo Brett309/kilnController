0e589af304f6ff97afa4858a403edc3f169827b0
Brett309 <55987616+Brett309@users.noreply.github.com>
2020-03-06 19:25:13 +1100

    Super Stablization
    Insulation Properties different kilns will require a little adjusting

--- lib/oven.py
+++ lib/oven.py
@@ -127,8 +127,9 @@ class Oven (threading.Thread):
                         temperature_count = 0
                     # If the heat is on and nothing is changing, reset
                     # The direction or amount of change does not matter
-                    # This prevents runaway in the event of a sensor read failure                   
-                    if temperature_count > 30:
+                    # This prevents runaway in the event of a sensor read failure 
+                    # Temp Count Long run Stabilization adjust below if your run ends before finishing adjust above below number
+                    if temperature_count > 100:
                         log.info("Error reading sensor, oven temp not responding to heat.")
                         self.reset()
                 else:
